e08dcf3c2a3570850aded5699fd3afba121e0cda
Omindu <it23575776@my.sliit.lk>
2026-03-06 01:02:53 +0530

    deployed code

--- server/tmsserver/Authcontroller.cs
+++ server/tmsserver/Authcontroller.cs
@@ -10,8 +10,6 @@ using tmsserver.Models;
 using tmsserver.Services;
 using AuthSvc = tmsserver.Services.IAuthorizationService;
 
-namespace tmsserver.Controllers;
-
 [Route("api/[controller]")]
 [ApiController]
 public class AuthController : ControllerBase
